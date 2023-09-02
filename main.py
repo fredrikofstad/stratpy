615aef7f54972ca18038b10ec76423172c9a122a
fredrikofstad <nokotin@gmail.com>
2023-09-02 14:13:34 +0200

    added game

--- main.py
+++
@@ -1,5 +0,0 @@
-import stratpy
-
-# calling functions from rust
-
-print(stratpy.sum_as_string(5,20))
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 use pyo3::prelude::*;
 
+mod game;
+
 /// Formats the sum of two numbers as string.
 #[pyfunction]
 fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
@@ -11,4 +13,14 @@ fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
 fn stratpy(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+  
+    #[test]
+    fn simple_test() {
+        assert_eq!(sum_as_string(2,2).unwrap(), "4");
+    }
 }
